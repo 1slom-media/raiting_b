923e69f8b81328682bcbd7e5dc73e52367e5c371
1slom_media <islombektagayev1@gmail.com>
2023-01-13 19:35:41 +0500

    add env

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ const pgConfig = {
     host: process.env.PG_HOST,
     database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
-    // port: process.env.PG_PORT,
+    port: process.env.PG_PORT,
 };
 
 export {
