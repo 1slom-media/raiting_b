032364563552a0b4a1949c0eec8f7f7bbee3e223
1slom_media <islombektagayev1@gmail.com>
2023-01-14 15:48:08 +0500

    delete newUser

--- src/modules/users/controller.js
+++ src/modules/users/controller.js
@@ -3,18 +3,18 @@ import jwt from '../../lib/jwt.js'
 import { AuthorizationError, ValidationError } from '../../utils/errors.js'
 import { fetchAll } from '../../lib/postgres.js'
 
-const allUsers = await fetchAll(`
- select
-    user_id,
-    companyname,
-    inn,
-    email,
-    ogrn,
-    kpp,
-    country
-from users as u
-where  case when $1 > 0 then u.user_id = $1 else true end
-order by u.user_id`,[0])
+// const allUsers = await fetchAll(`
+//  select
+//     user_id,
+//     companyname,
+//     inn,
+//     email,
+//     ogrn,
+//     kpp,
+//     country
+// from users as u
+// where  case when $1 > 0 then u.user_id = $1 else true end
+// order by u.user_id`,[0])
 
 const GET = async (req, res) => {
     try {
@@ -51,14 +51,14 @@ const LOGIN = async (req, res,next) => {
 const REGISTER=async (req,res,next)=>{
     try {
         const user = await model.REGISTERMODEL(req.body)
-        let newUser;
-        allUsers.filter(use=>{
-            if(use.companyname !== user.companyname){
-                return newUser = true
-            }
-            return newUser = false
-        })
-        if(newUser) {
+        // let newUser;
+        // allUsers.filter(use=>{
+        //     if(use.companyname !== user.companyname){
+        //         return newUser = true
+        //     }
+        //     return newUser = false
+        // })
+        if(user) {
             res.status(201).json({
               status: 201,
               message: "ok",
