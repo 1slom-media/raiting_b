b22d2688e63a56146a6f421f763ec59e50a009ab
1slom_media <islombektagayev1@gmail.com>
2023-08-04 09:43:07 +0500

    endi aniq bo`ldi

--- src/modules/banks/model.js
+++ src/modules/banks/model.js
@@ -1,37 +1,39 @@
 import { fetch, fetchAll } from "../../lib/postgres.js";
+import pkg from "lodash";
+const { uniqBy } = pkg;
 
 import { GETBANKS, POSTBANKS, PUTBANKS, DELETEBANKS } from "./query.js";
 
 const GET = async ({ bankId = 0 }, { search }) => {
-    try {
-      const banks = await fetchAll(GETBANKS, [bankId, search]);
-  
-      const bankIds = banks.map((bank) => bank.bank_id);
-  
-      const getRatingsQuery = `
-        SELECT *
-        FROM raiting
-        WHERE bank_id = ANY($1)
-        ORDER BY updated_at DESC
-      `;
-  
-      const ratings = await fetchAll(getRatingsQuery, [bankIds]);
-  
-      const uniqueBanks = banks.map((bank) => {
-        const bankRatings = ratings.filter(
-          (rating) => rating.bank_id === bank.bank_id
-        );
-        return {
-          ...bank,
-          raiting: bankRatings.length > 0 ? bankRatings : [],
-        };
-      });
-  
-      return uniqueBanks;
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  try {
+    const banks = await fetchAll(GETBANKS, [bankId, search]);
+
+    const bankIds = banks.map((bank) => bank.bank_id);
+
+    const getRatingsQuery = `
+      SELECT *
+      FROM raiting
+      WHERE bank_id = ANY($1)
+      ORDER BY id, updated_at DESC
+    `;
+
+    const ratings = await fetchAll(getRatingsQuery, [bankIds]);
+
+    const uniqueBanks = uniqBy(banks, 'bank_id');
+    const uniqueRatings = uniqBy(ratings, 'id');
+
+    const banksWithRatings = uniqueBanks.map((bank) => ({
+      ...bank,
+      raiting: uniqueRatings.filter((rating) => rating.bank_id === bank.bank_id),
+    }));
+
+    return banksWithRatings;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
 
 const POST = async ({ category_id, companyname, inn, ogrn, kpp, country,country_uz,country_en }) => {
   try {
